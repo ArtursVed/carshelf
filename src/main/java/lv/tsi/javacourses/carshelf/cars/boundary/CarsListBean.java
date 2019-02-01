e89175132a5d5411b356ba710df13b7f3e21b34b
ArtursVed <varturs81@gmail.com>
2019-02-01 12:43:34 +0200

    car__new_one_01022019

--- src/main/java/lv/tsi/javacourses/carshelf/cars/boundary/CarsListBean.java
+++ src/main/java/lv/tsi/javacourses/carshelf/cars/boundary/CarsListBean.java
@@ -3,6 +3,7 @@ package lv.tsi.javacourses.carshelf.cars.boundary;
 import lv.tsi.javacourses.carshelf.cars.model.CarEntity;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,15 +14,14 @@ import java.util.List;
 
 
 @Named
-@SessionScoped
+@ViewScoped
 public class CarsListBean implements Serializable {
     @PersistenceContext
     private EntityManager em;
     private String term;
     private List<String> cars;
     private Long id;
-
-
+    private boolean delete = false;
 
     public List<CarEntity> getCar() {
         if (term == null) {
@@ -33,15 +33,21 @@ public class CarsListBean implements Serializable {
         }
     }
 
-    @Transactional
-    public void deletecars(Long id) {
-        Query query = em.createQuery("DELETE FROM Car c WHERE c.id =" + id);
-        query.executeUpdate();
+  // @Transactional
+  //  public void deletecars(Long id) {
+  //    Query query = em.createQuery("DELETE FROM Car c WHERE c.id =" + id);
+  //       query.executeUpdate();
+ //   }
+
+
+     @Transactional
+    public String deletecars () {
+    CarEntity car;
+    car =em.find(CarEntity .class,id);
+    em.remove(car);
+    delete = true;
+    return null;
     }
-
-
-
-
 
     public List<String> getCars() {
         return cars;
@@ -77,6 +83,14 @@ public class CarsListBean implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public boolean isDelete() {
+        return delete;
+    }
+
+    public void setDelete(boolean delete) {
+        this.delete = delete;
     }
 }
 
